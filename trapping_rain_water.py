8cad519c9a3fda63908c4a8722396cd4cebcf506
Navid Fayezi <navidfayezi.98@gmail.com>
2025-10-24 17:24:29 -0600

    Add solution to rain water

---
+++ trapping_rain_water.py
@@ -0,0 +1,30 @@
+class Solution:
+    def trap(self, height: list[int]) -> int:
+        array_length = len(height)
+        
+        # This loop finds the left max of each element
+        left_maxes =  [height[0]]
+        for i in range(array_length - 1):
+            if height[i] > left_maxes[-1]:
+                left_maxes.append(height[i])
+            else:
+                left_maxes.append(left_maxes[-1])
+        
+        index = array_length - 1
+        right_max = height[index]
+        rain = 0
+        while index >= 0:
+            temp = min(right_max, left_maxes[index]) - height[index] 
+            if temp > 0:
+                rain += temp
+            if height[index] > right_max:
+                right_max = height[index]
+            index -= 1
+        return rain
+        
+        
+
+if __name__ == "__main__":
+    test_input = [4,2,0,3,2,5]
+    solution = Solution()
+    print(solution.trap(test_input))
