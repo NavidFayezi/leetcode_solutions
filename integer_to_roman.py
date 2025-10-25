887c6972421f673cc97a789bb967c89318fdb310
Navid Fayezi <navidfayezi.98@gmail.com>
2025-10-25 15:43:53 -0600

    Modify the solution of integer to roman to adhere to python coding style

--- integer_to_roman.py
+++ integer_to_roman.py
@@ -1,11 +1,26 @@
 class Solution:
-    mapping = [("M", 1000), ("D", 500), ("C", 100), ("L", 50), ("X", 10), ("V", 5), ("I", 1)]
-    subtractive_mapping = [("CM", 900), ("CD", 400), ("XC", 90), ("XL", 40), ("IX", 9), ("IV", 4)]
-    
+    mapping = [
+        ("M", 1000),
+        ("D", 500),
+        ("C", 100),
+        ("L", 50),
+        ("X", 10),
+        ("V", 5),
+        ("I", 1),
+    ]
+    subtractive_mapping = [
+        ("CM", 900),
+        ("CD", 400),
+        ("XC", 90),
+        ("XL", 40),
+        ("IX", 9),
+        ("IV", 4),
+    ]
+
     def intToRoman(self, num: int) -> str:
-        assert(num > 0)
-        assert(num < 4000)
-        
+        assert num > 0
+        assert num < 4000
+
         # First, break the number into its decimal places
         divider = 10
         decimal_places = []
@@ -14,15 +29,15 @@ class Solution:
             decimal_places.append((num % divider) * multiplier)
             multiplier *= 10
             num = num // 10
-        
+
         # Then, convert each decimal place to roman numeral, using the helper
         # function, and concatenate the results
         roman_numeral = ""
         for i in decimal_places:
             roman_numeral = self.helper(i) + roman_numeral
 
-        return roman_numeral 
-    
+        return roman_numeral
+
     def helper(self, num: int) -> str:
         if num == 0:
             return ""
@@ -32,13 +47,13 @@ class Solution:
                 return map[0]
 
         num_letter = len(self.mapping)
-        current_letter = -1 
+        current_letter = -1
         for i in range(num_letter):
             if self.mapping[i][1] <= num:
                 current_letter = self.mapping[i]
                 break
-        
-        assert(current_letter != -1)
+
+        assert current_letter != -1
         return current_letter[0] + self.helper(num - current_letter[1])
 
 
