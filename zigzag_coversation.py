15132d210e3b36807fc914a586400661ba80b91b
Navid Fayezi <navidfayezi.98@gmail.com>
2025-10-27 15:01:50 -0600

    Fix formatting of zigzag_conversation.py

--- zigzag_coversation.py
+++ zigzag_coversation.py
@@ -22,14 +22,14 @@ class Solution:
         direction = 0
         while index < str_length:
             column_counter = column_counter % numRows
-            
+
             # this prevents double full columns
             if column_counter == numRows - 1:
                 column_counter = 0
-            
+
             # this is the column you can fill completely
             if column_counter == 0:
-                
+
                 # the for loop fills a column
                 temp = ["" for i in range(numRows)]
                 for i in range(numRows):
@@ -38,10 +38,10 @@ class Solution:
                     else:
                         temp[i] = s[index]
                         index += 1
-                
+
                 zigzag_matrix.append(temp)  # append the column to the matrix
                 column_counter += 1
-            
+
             # this is the column that should only contain one letter
             else:
                 if direction == 0:
@@ -54,9 +54,12 @@ class Solution:
                             zigzag_matrix.append(temp)
                             index += 1
                             column_counter = (column_counter + 1) % numRows
-                            if column_counter == 0 or column_counter == numRows - 1:
+                            if (
+                                column_counter == 0
+                                or column_counter == numRows - 1
+                            ):
                                 break
-                    
+
                 else:
                     for i in range(1, numRows - 1):
                         if index >= str_length:
@@ -67,11 +70,13 @@ class Solution:
                             zigzag_matrix.append(temp)
                             index += 1
                             column_counter = (column_counter + 1) % numRows
-                            if column_counter == 0 or column_counter == numRows - 1:
+                            if (
+                                column_counter == 0
+                                or column_counter == numRows - 1
+                            ):
                                 break
-        
+
         return self.zigzag_matrix_to_str(zigzag_matrix, numRows)
-    
 
     def zigzag_matrix_to_str(self, zigzag: list, numrows: int) -> str:
         """Flatten a column-oriented zigzag matrix into a single string.
@@ -90,7 +95,7 @@ class Solution:
         for i in range(numrows):
             for chars in zigzag:
                 res = res + chars[i]
-        
+
         return res
 
 
