6bba82360d60fae7d95a24f4af7539aadae73eea
Navid <navidfayezi.98@gmail.com>
2026-09-20 19:52:54 -0600

    Add a more efficient DC solution to the Merge k Sorted Lists problem

--- merge_k_sorted_lists.py
+++ merge_k_sorted_lists.py
@@ -7,26 +7,33 @@ class Solution:
     def mergeKLists(self, lists: list[ListNode | None]) -> ListNode | None:
         if lists == []:
             return None
-        
+
+        if len(lists) == 1:
+            return lists[0]
+
         else:
+            middle = len(lists) // 2
+            list1 = self.mergeKLists(lists[:middle])
+            list2 = self.mergeKLists(lists[middle:])
+            my_lists = [list1, list2]
             res = ListNode()
             res_end = res
             incomplete = True
+            min_node = ListNode(10**5)
             while incomplete == True:
                 incomplete = False
-                temp_min = ListNode(10**5)
-                no_lists = len(lists)
+                temp_min = min_node
                 min_index = -1
-                for i in range(no_lists):
-                    if lists[i] is not None:
+                for i in range(2):
+                    if my_lists[i] is not None:
                         incomplete = True
-                        if lists[i].val <= temp_min.val:
-                            temp_min = lists[i]
+                        if my_lists[i].val <= temp_min.val:
+                            temp_min = my_lists[i]
                             min_index = i
                 
                 if incomplete == True:
-                    lists[min_index] = lists[min_index].next
+                    my_lists[min_index] = my_lists[min_index].next
                     res_end.next = temp_min
                     res_end = res_end.next
             
-            return res.next  
+            return res.next
